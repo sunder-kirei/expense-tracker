a52582d2258e86ee21e7de9e4ae592b1a9684ef4
sunder-kirei <sunder-kirei@outlook.com>
2024-10-07 14:35:22 +0530

    update readme and add env type checking

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,8 @@
-namespace NodeJS {
-  interface ProcessEnv {}
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string;
+    AUTH_SECRET: string;
+    AUTH_GOOGLE_ID: string;
+    AUTH_GOOGLE_SECRET: string;
+  }
 }
